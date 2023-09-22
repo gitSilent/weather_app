e9adebdb2d33cd000196c6f2153b846e2ae02562
gitSilent <97840080+gitSilent@users.noreply.github.com>
2023-09-22 10:30:57 +0300

    basic weather render + scroll

--- components/DayWeatherCard.js
+++ components/DayWeatherCard.js
@@ -1,17 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function DayWeatherCard({ maxTemp, minTemp, phrase, date, icon }) {
     let newDate = new Date(date);
-    console.log(newDate.toDateString());
-    console.log(newDate.toISOString());
-    console.log(newDate.toLocaleDateString("ru-RU"));
+    let day = newDate.getDate()
+    console.log(newDate.getDate());
+    let month = newDate.toLocaleDateString("ru-RU",{ month: 'short' })
+
     return (
         <View style={styles.container}>
-            <Text>{newDate.toDateString()}</Text>
-            <Text>Макс.t°{maxTemp}</Text>
-            <Text>Мин.t°{minTemp}</Text>
-            <Text>{phrase}</Text>
+            <Text style={{fontSize: "20px", fontWeight: "400"}}>{day} {month}</Text>
+            <Image style={{width:"50px", height:"50px"}} source={require('../media/icon.svg')}/>
+            <Text>Макс.t° {maxTemp}</Text>
+            <Text>Мин.t° {minTemp}</Text>
+            <Text style={styles.text}>{phrase}</Text>
         </View>
     );
 }
@@ -20,13 +21,18 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#c2cfed",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "fit-content",
-        width: "fit-content",
-        maxHeight: "250px",
-        maxWidth: "250px",
+        height: "200px",
+        width: "150px",
         borderColor: "#7d7d7d",
         borderRadius: "20px",
+        padding:"10px",
+        marginLeft:'5px',
+        marginRight:'5px',
     },
+    text:{
+      position:'absolute',
+      bottom:'15px',
+      left:'10px',
+      color:'#5e5e5e'
+    }
 });
--- pages/MainPage.js
+++ pages/MainPage.js
@@ -3,10 +3,11 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import DayWeatherCard from "../components/DayWeatherCard";
+import { ScrollView } from "react-native";
 
 export default function MainPage() {
     const [currentWeather, setCurrentWeather] = useState({});
-    const [weather5Days, setWeather5Days] = useState({});
+    const [weather5Days, setWeather5Days] = useState([]);
 
     useEffect(() => {
         axios.get("http://dataservice.accuweather.com/currentconditions/v1/2512351?apikey=5c69t6JDGErUgcvSGKElYGM5Adicsdoc&language=ru").then((resp) => {
@@ -15,22 +16,27 @@ export default function MainPage() {
         });
         axios.get("http://dataservice.accuweather.com/forecasts/v1/daily/5day/2512351?apikey=5c69t6JDGErUgcvSGKElYGM5Adicsdoc&language=ru&metric=true").then((resp) => {
             console.log(resp);
-            setWeather5Days(resp.data);
+            setWeather5Days(resp.data.DailyForecasts);
         });
     }, []);
 
     return (
         <View style={[styles.container, { backgroundColor: currentWeather?.IsDayTime ? "#6a90eb" : "#102352" }]}>
             {/* <View style={[styles.container, {backgroundColor: currentWeather?.IsDayTime ? "#6a90eb" : "#102352"}]}> */}
-            <View style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
-                <Text style={[{ fontSize: "35px", fontWeight: "500" }, currentWeather?.IsDayTime ? styles.textDay : styles.textNight]}>{currentWeather?.Temperature?.Metric?.Value}</Text>
-                <Text style={[{ fontSize: "35px", fontWeight: "500" }, currentWeather?.IsDayTime ? styles.textDay : styles.textNight]}>{currentWeather?.Temperature?.Metric?.Unit}</Text>
+            <View style={{marginLeft:'10px'}}>
+              <View style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
+                  <Text style={[{ fontSize: "55px", fontWeight: "500" }, currentWeather?.IsDayTime ? styles.textDay : styles.textNight]}>{currentWeather?.Temperature?.Metric?.Value}</Text>
+                  <Text style={[{ fontSize: "55px", fontWeight: "500" }, currentWeather?.IsDayTime ? styles.textDay : styles.textNight]}>{currentWeather?.Temperature?.Metric?.Unit}</Text>
+              </View>
+              <Text style={[{ fontSize: "25px", fontWeight: "500" }, currentWeather?.IsDayTime ? styles.textDay : styles.textNight]}>{currentWeather?.WeatherText}</Text>
             </View>
-            <Text style={[{ fontSize: "15px", fontWeight: "500" }, currentWeather?.IsDayTime ? styles.textDay : styles.textNight]}>{currentWeather?.WeatherText}</Text>
 
-            <View>
-              <DayWeatherCard maxTemp={20} minTemp={10} phrase={'облачно'} date={"2023-09-21T07:00:00+03:00"} icon={""}/>
-            </View>
+            <ScrollView horizontal={true} style={{maxWidth:'100%', marginTop:'50px'}}  >
+              {weather5Days?.map((item)=>{
+                return <DayWeatherCard maxTemp={item.Temperature.Maximum.Value} minTemp={item.Temperature.Minimum.Value} phrase={item.Day.IconPhrase} date={item.Date} icon={""}/>
+              })}
+
+            </ScrollView>
             <StatusBar style="auto" />
         </View>
     );
@@ -39,8 +45,10 @@ export default function MainPage() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        // justifyContent:'center',
+        // alignContent:'center',
+        paddingTop:'50px',
+
     },
     textDay: {
         color: "black",
